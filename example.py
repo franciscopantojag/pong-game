478b1cf9dbfa1316c913b6a8bb22a4c87e228fd5
Francisco <franciscopantojag98@gmail.com>
2020-08-20 19:07:09 -0500

    Initial Commit

---
+++ example.py
@@ -0,0 +1,92 @@
+# First attempt at Python
+print ("Hola") 
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Pantoja")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+#ball
+ball= turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dix = 0.1
+ball.diy = 0.1
+#ball Movement
+
+#Function
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_down,"Down")
+wn.onkeypress(paddle_b_up,"Up")
+#Main game loop
+while True:
+    wn.update()     
+    #ball movement
+    ball.setx(ball.xcor() + ball.dix)
+    ball.sety(ball.ycor() + ball.diy)
+    #Border check
+    if ball.ycor()>290:
+        ball.sety(290)
+        ball.diy*=-1
+    if ball.ycor()<-290:
+        ball.sety(-290)
+        ball.diy*=-1
+    if ball.xcor()>390:
+        ball.goto(0,0)
+        ball.dix*=-1
+    if ball.xcor()<-390:
+        ball.goto(0,0)
+        ball.dix*=-1
+    #Paddle check
+    if ball.xcor()>340 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor()>paddle_b.ycor()-50):
+        ball.dix*=-1
+    if ball.xcor()<-340 and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor()>paddle_a.ycor()-50):
+        ball.dix*=-1
+    if paddle_a.ycor()>250:
+        paddle_a.goto(-350,250)
+    if paddle_a.ycor()<-250:
+        paddle_a.goto(-350,-250)
+    if paddle_b.ycor()>250:
+        paddle_b.goto(350,250)
+    if paddle_b.ycor()<-250:
+        paddle_b.goto(350,-250)
